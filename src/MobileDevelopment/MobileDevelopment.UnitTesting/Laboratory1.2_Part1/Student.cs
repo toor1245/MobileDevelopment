bdde1bbe019be405ab87add147438cfabc89d4f9
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-02-08 03:21:46 +0200

    remove folders

--- src/MobileDevelopment/MobileDevelopment.UnitTesting/Laboratory1.2_Part1/Student.cs
+++
@@ -1,8 +0,0 @@
-namespace MobileDevelopment.UnitTesting.Laboratory1._2_Part1
-{
-    public class Student
-    {
-        public string Name { get; set; }
-        public string Group { get; set; }
-    }
-}
